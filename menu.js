b4f83272790f27d3ab60f643681090709812b677
Diona Moss <123892824+D-Moss@users.noreply.github.com>
2025-02-10 00:51:54 -0500

    Add files via upload
    changed the side navigation menu to full screen overlay. updated the starter page

--- menu.js
+++ menu.js
@@ -1,13 +1,7 @@
-function openMenu(evt, menuName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(menuName).style.display = "block";
-	evt.currentTarget.className += " active";
+function openNav() {
+	document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
 }
